4d41a2017e038608285ea445a65e984717f477de
AzharMoosaOdin <captainminer767@gmail.com>
2019-08-05 20:21:12 +0100

    HTML and CSS added

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+// Operations
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function divide(num1, num2) {
+	return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+	if (operator === "add") {
+		add(num1, num2) 
+	} else if (operator === "subtract") {
+		subtract(num1, num2);
+	} else if (operator === "multiply") {
+		multiply(num1, num2);
+	} else if (operator === "divide") {
+		divide(num1, num2);
+	}
+}
