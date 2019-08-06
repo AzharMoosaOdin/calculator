75943ed93d64b861ed9672df5b7fbbd6d06547e0
AzharMoosaOdin <captainminer767@gmail.com>
2019-08-06 11:03:47 +0100

    Numbers appear on display

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,47 @@
-// Operations
-function add(num1, num2) {
-	return num1 + num2;
+let screen_text = document.querySelector("#display p");
+
+// Clear Button
+function ac() {
+	screen_text.textContent = "0";
 }
 
-function subtract(num1, num2) {
-	return num1 - num2;
+// Plus-Minus
+function plus_minus() {
+	let number = screen_text.textContent;
+	if (number > 0) {
+		screen_text.textContent = -number
+	} else {
+		screen_text.textContent = Math.abs(number);
+	}
 }
 
-function multiply(num1, num2) {
-	return num1 * num2;
+// Backspace
+function backspace() {
+	let number = screen_text.textContent;
+	let newNum = number.slice(0, -1);
+	if (newNum === "") {
+		screen_text.textContent = "0";
+	} else {
+		screen_text.textContent = newNum;
+	}
 }
 
-function divide(num1, num2) {
-	return num1 / num2;
-}
+// Numbers
+function numbers() {
+	let current = screen_text.textContent;
+	if (current.length >= 15) {
+		return;
+	}
+	let number = event.target.id;
+	if (number === "zero") {
+		number = 0;
+	}
+	if (screen_text.textContent === "0") {
+		screen_text.textContent = "";
+	}
+	screen_text.textContent += number;
 
-function operate(operator, num1, num2) {
-	if (operator === "add") {
-		add(num1, num2) 
-	} else if (operator === "subtract") {
-		subtract(num1, num2);
-	} else if (operator === "multiply") {
-		multiply(num1, num2);
-	} else if (operator === "divide") {
-		divide(num1, num2);
+	if (current.length % 5 === 0) {
+		screen_text.style.fontSize = 60 - (current.length + 10) + "px";
 	}
 }
